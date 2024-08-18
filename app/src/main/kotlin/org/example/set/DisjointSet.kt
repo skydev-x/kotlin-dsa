b550d9b799fcdae8c904d8e2cf630982614e21bb
dukendev <ysanjeet535@gmail.com>
2024-08-18 20:04:55 +0530

    minor

--- app/src/main/kotlin/org/example/set/DisjointSet.kt
+++ app/src/main/kotlin/org/example/set/DisjointSet.kt
@@ -8,11 +8,22 @@ class DisjointSet(size: Int) {
     val pa = find(a)
     val pb = find(b)
     if (pa == pb) return
-    parent[pb] = a
+    if (rank[pa] > rank[pb]) {
+      parent[pb] = pa
+    } else if (rank[pa] < rank[pb]) {
+      parent[pa] = pb
+    } else {
+      parent[pa] = pb
+      rank[pb]++
+    }
   }
 
   fun find(a: Int): Int {
-    if (a == parent[a]) return a
-    return find(parent[a])
+    // if (a == parent[a]) return a
+    // return find(parent[a])
+    if (parent[a] != a) {
+      parent[a] = find(parent[a])
+    }
+    return parent[a]
   }
 }
